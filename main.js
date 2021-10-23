f5b7d8e995f5b30d580b0ab2253a781d9ca43257
Tamara Ilic <tamara@ilicmcfarlane.io>
2021-10-23 12:46:40 +0200

    add functionality to mark todo as done WIP

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+let id = 1;
+
 function getInputValue() {
-    // Selecting the input element and get its value 
-    let inputVal = document.getElementById("myInput").value;
-    let ul = document.getElementById("list");
-    let liElement = document.createElement('li');
-    liElement.appendChild(document.createTextNode(inputVal));
-    ul.appendChild(liElement);
+  // Selecting the input element and get its value
+  let inputVal = document.getElementById("myInput").value;
+  let ul = document.getElementById("list");
+  let liElement = document.createElement("li");
+  liElement.appendChild(document.createTextNode(inputVal));
+  ul.appendChild(liElement);
+  liElement.classList.add("task" + id++); // delete?
+  liElement.addEventListener("click", (e) => toggleTaskDone(i));
+}
+
+// Select all future list items
+let tasks = document.querySelectorAll("li");
+tasks.forEach((task, i) => {
+  console.log(i);
+  task.addEventListener("click", (e) => toggleTaskDone(i));
+});
+
+// Toggle class name
+function toggleTaskDone(i) {
+  tasks[i].classList.toggle("task-done");
 }
