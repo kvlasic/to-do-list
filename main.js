a01039f9e3062ea033a57e13f8cf4900b2bce340
Tamara Ilic <tamara@ilicmcfarlane.io>
2021-10-23 13:23:50 +0200

    add functionality to mark dynamically created list items as done

--- main.js
+++ main.js
@@ -3,22 +3,15 @@ let id = 1;
 function getInputValue() {
   // Selecting the input element and get its value
   let inputVal = document.getElementById("myInput").value;
-  let ul = document.getElementById("list");
-  let liElement = document.createElement("li");
-  liElement.appendChild(document.createTextNode(inputVal));
-  ul.appendChild(liElement);
-  liElement.classList.add("task" + id++); // delete?
-  liElement.addEventListener("click", (e) => toggleTaskDone(i));
+  let toDoList = document.getElementById("list");
+
+  let task = document.createElement("li");
+  task.appendChild(document.createTextNode(inputVal));
+  toDoList.appendChild(task);
+  task.addEventListener("click", () => toggleTaskDone(task));
 }
 
-// Select all future list items
-let tasks = document.querySelectorAll("li");
-tasks.forEach((task, i) => {
-  console.log(i);
-  task.addEventListener("click", (e) => toggleTaskDone(i));
-});
-
 // Toggle class name
-function toggleTaskDone(i) {
-  tasks[i].classList.toggle("task-done");
+function toggleTaskDone(task) {
+  task.classList.toggle("task-done");
 }
