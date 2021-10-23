a3b51f109ae5b7047cdaf54764108184f832fc7d
Tamara Ilic <tamara@ilicmcfarlane.io>
2021-10-23 14:02:22 +0200

    add project task list

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-let id = 1;
+/*
+MUST HAVE
+ 1. Create a todo from user input
+ 2. Mark todo as done
+ 3. Delete a todo
+ 4. Edit a todo
+COULD HAVE
+ 5. Store list in local storage
+ 6. 'Enter' has same functionality as clicking 'submit' button
+ 7. User input is cleared after it is added to list
+NICE TO HAVE 
+ 8. Reset list
+ 9. List category / title
+*/
 
 function getInputValue() {
   // Selecting the input element and get its value
@@ -11,7 +24,7 @@ function getInputValue() {
   task.addEventListener("click", () => toggleTaskDone(task));
 }
 
-// Toggle class name
+// Toggle class
 function toggleTaskDone(task) {
   task.classList.toggle("task-done");
 }
