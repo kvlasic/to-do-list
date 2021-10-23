da15655c5c675a20217b85e26a4eedd9bb53736c
Kristijan Vlasic <kristijan.vlasic1@gmail.com>
2021-10-23 14:51:36 +0200

    fixed adding items and remove button

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+// Add items based on user input
 function getInputValue() {
-    // Selecting the input element and get its value 
-    let inputVal = document.getElementById("myInput").value;
-    let ul = document.getElementById("list");
-    let liElement = document.createElement('li');
-    liElement.appendChild(document.createTextNode(inputVal));
-    ul.appendChild(liElement);
+    let inputVal = document.getElementById("myInput");
+    const ul = document.getElementById("list");
+    const liElement = document.createElement('li');
+    liElement.appendChild(document.createTextNode(inputVal.value));
+    // If user doesn't write anything he gets a warning alert
+    if (inputVal === '') {
+        alert("You must write something!");
+    } else {
+        ul.appendChild(liElement);
+        inputVal.value = ""; // removes text from input field after being submitted
+    }
+
+    // Add X to remove items
+    let span = document.createElement("span");
+    let xSymbol = document.createTextNode("\u00D7"); // \u00D7 is unicode for x symbol I guess
+    span.className = "delete"; // span element gets a class named delete
+    span.appendChild(xSymbol);
+    liElement.appendChild(span);
 }
