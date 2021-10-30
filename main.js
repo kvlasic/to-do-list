f614787719959535fd609977366183a960fa4326
Tamara Ilic <tamara@ilicmcfarlane.io>
2021-10-30 09:24:54 +0200

    add functionality to add tasks dynamically

--- main.js
+++ main.js
@@ -15,30 +15,46 @@ NICE TO HAVE
 */
 
 // Add items based on user input
-function getInputValue() {
-  let inputVal = document.getElementById("myInput");
-  const todoList = document.getElementById("list");
-  const task = document.createElement("li");
-  task.appendChild(document.createTextNode(inputVal.value));
-  // If user doesn't write anything he gets a warning alert
-  if (inputVal === "") {
-    alert("You must write something!");
-  } else {
-    todoList.appendChild(task);
-    inputVal.value = ""; // removes text from input field after being submitted
-  }
+// function getInputValue() {
+//   let inputVal = document.getElementById("myInput");
+//   const todoList = document.getElementById("list");
+//   const task = document.createElement("li");
+//   task.appendChild(document.createTextNode(inputVal.value));
+//   // If user doesn't write anything he gets a warning alert
+//   if (inputVal === "") {
+//     alert("You must write something!");
+//   } else {
+//     todoList.appendChild(task);
+//     inputVal.value = ""; // removes text from input field after being submitted
+//   }
 
-  task.addEventListener("click", () => toggleTaskDone(task));
+//   task.addEventListener("click", () => toggleTaskDone(task));
 
-  // Add X to remove items
-  let span = document.createElement("span");
-  let xSymbol = document.createTextNode("\u00D7"); // \u00D7 is unicode for x symbol I guess
-  span.className = "delete"; // span element gets a class named delete
-  span.appendChild(xSymbol);
-  task.appendChild(span);
+//   // Add X to remove items
+//   let span = document.createElement("span");
+//   let xSymbol = document.createTextNode("\u00D7"); // \u00D7 is unicode for x symbol I guess
+//   span.className = "delete"; // span element gets a class named delete
+//   span.appendChild(xSymbol);
+//   task.appendChild(span);
+// }
+
+// // Toggle class
+// function toggleTaskDone(task) {
+//   task.classList.toggle("task-done");
+// }
+
+let todoListTasks = [];
+
+function addNewTask(event) {
+  const newItem = event.target.elements["new-item"];
+  todoListTasks.unshift(newItem.value); // check whether prepend works instead
+  newItem.value = "";
+  updateTheScreen();
 }
 
-// Toggle class
-function toggleTaskDone(task) {
-  task.classList.toggle("task-done");
+function updateTheScreen() {
+  todoList.innerHTML = "";
+  todoListTasks.forEach((item, index) => {
+    todoList.innerHTML += `<li></li>`;
+  });
 }
